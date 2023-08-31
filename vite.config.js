2c29d7d2c860daa4c6af12b023936fb7762a3564
Lautaro <epifanilautaro@gmail.com>
2023-08-31 14:00:12 +0200

    gh page

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://lautaroepifani.github.io/Portfolio/",
+  base: "https://lautaroepifani.github.io/Portfolio/src/index.html",
   plugins: [react()],
   assetsInclude: ['**/*.mp4'],
 })
